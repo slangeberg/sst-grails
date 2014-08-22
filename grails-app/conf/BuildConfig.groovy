a5983dbadc2413d9ff0b045da0716a6180726648
Scott Langeberg <slangeberg@gmail.com>
2014-08-22 14:55:56 -0500

    Added coverage plugin - use with  test-app -coverage; Un-forked test run, for Intellij

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,74 +8,76 @@ grails.project.source.level = 1.6
 grails.project.war.file = "target/${appName}.war"
 
 grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+   // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+   //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
-    // Intellij does not debug with forked process, by default
-    test: false,  //[maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+   // Intellij does not debug with forked process, by default
+   test: false,  //[maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+   // run-app JVM
+   run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+   // run-war JVM
+   war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+   // Console UI JVM
+   console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+   // inherit Grails' default dependencies
+   inherits("global") {
+      // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+      // excludes 'ehcache'
+   }
+   log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+   checksums true // Whether to verify checksums on resolve
+   legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
-    repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+   repositories {
+      inherits true // Whether to inherit repository definitions from plugins
 
-        grailsPlugins()
-        grailsHome()
-        mavenLocal()
-        grailsCentral()
-        mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
+      grailsPlugins()
+      grailsHome()
+      mavenLocal()
+      grailsCentral()
+      mavenCentral()
+      // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+      //mavenRepo "http://repository.codehaus.org"
+      //mavenRepo "http://download.java.net/maven/2/"
+      //mavenRepo "http://repository.jboss.com/maven2/"
+   }
 
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        // runtime 'mysql:mysql-connector-java:5.1.29'
-        // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
+   dependencies {
+      // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+      // runtime 'mysql:mysql-connector-java:5.1.29'
+      // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
 
       compile 'joda-time:joda-time:2.4'
       compile 'org.apache.commons:commons-lang3:3.3.2'
 
       test 'cglib:cglib-nodep:3.1'
       test 'org.grails:grails-datastore-test-support:1.0-grails-2.4'
-    }
+   }
 
-    plugins {
-        // plugins for the build system only
-        build ":tomcat:7.0.55"
+   plugins {
+      // plugins for the build system only
+      build ":tomcat:7.0.55"
 
-        // plugins for the compile step
-        compile ":scaffolding:2.1.2"
-        compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.6"
+      // plugins for the compile step
+      compile ":scaffolding:2.1.2"
+      compile ':cache:1.1.7'
+      compile ":asset-pipeline:1.9.6"
 
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
-        runtime ":database-migration:1.4.0"
-        runtime ":jquery:1.11.1"
+      // plugins needed at runtime but not for compilation
+      runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+      runtime ":database-migration:1.4.0"
+      runtime ":jquery:1.11.1"
 
-        // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.9.0"
-        //compile ":less-asset-pipeline:1.10.0"
-        //compile ":coffee-asset-pipeline:1.8.0"
-        //compile ":handlebars-asset-pipeline:1.3.0.3"
-    }
+      test ":code-coverage:2.0.3-2"
+
+      // Uncomment these to enable additional asset-pipeline capabilities
+      //compile ":sass-asset-pipeline:1.9.0"
+      //compile ":less-asset-pipeline:1.10.0"
+      //compile ":coffee-asset-pipeline:1.8.0"
+      //compile ":handlebars-asset-pipeline:1.3.0.3"
+   }
 }
